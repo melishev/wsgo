ef25321c60326e4f5c4cdb63a9a809d018e1c726
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2024-02-10 17:00:52 +0100

    feat: add heartbeat mechanism

---
+++ test/utils.ts
@@ -0,0 +1,28 @@
+import ws from 'ws'
+
+export function createMockWSServer(port: number = 0): { server: ws.WebSocketServer; port: number } {
+  let server = new ws.WebSocketServer({ port })
+
+  server.on('connection', (ws) => {
+    ws.on('message', (data, isBinary) => {
+      data = isBinary ? data : JSON.parse(data.toString())
+
+      if (data.event === 'ping') {
+        const message = { event: 'pong', timeSended: Date.now() }
+        ws.send(JSON.stringify(message))
+      }
+
+      const message = { ...data, timeSended: Date.now() }
+
+      // setTimeout(() => {
+      //   ws.send(JSON.stringify(message))
+      // }, 1000)
+      ws.send(JSON.stringify(message))
+    })
+  })
+
+  return {
+    server,
+    port: (server.address() as ws.AddressInfo).port,
+  }
+}

3537bf64127c25c5aaf63833ae0d383db55e2e04
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2024-02-10 20:20:54 +0100

    refactor: move send logic to folder

--- src/send/index.ts
+++ src/send/index.ts
@@ -1,5 +1,5 @@
-import type { WSGOSendData } from './send/types'
-import type { WSGOEventName, WSGOConfig } from './types'
+import type { WSGOSendData } from './types'
+import type { WSGOEventName, WSGOConfig } from '../types'
 
 /** Method allows you to send an event to the server */
 export function send(ws: WebSocket, _config: WSGOConfig, eventName: WSGOEventName, data?: WSGOSendData): void {

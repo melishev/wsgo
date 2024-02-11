797e26adc02b576b60ffb81a26963b490e815c57
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2024-02-11 18:22:44 +0100

    style: format code

--- src/close.ts
+++ src/close.ts
@@ -1,7 +1,11 @@
 import { heartbeatStop } from './heartbeat'
 import type { WSGOConfig } from './types'
 
-export function close(ws: WebSocket, _config: WSGOConfig, ...[code = 1000, reason]: Parameters<WebSocket['close']>): void {
+export function close(
+  ws: WebSocket,
+  _config: WSGOConfig,
+  ...[code = 1000, reason]: Parameters<WebSocket['close']>
+): void {
   if (_config.heartbeat) {
     // stop heartbeat interval
     heartbeatStop()
